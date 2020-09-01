6d378fdec1662c9f4d73b0cf76ed6b76d10c7fec
yukimatsuri-01 <matsunariyukiko@gmail.com>
2020-09-01 18:11:27 +0900

    チャットルームの削除機能の実装

--- config/application.rb
+++ config/application.rb
@@ -9,15 +9,6 @@ Bundler.require(*Rails.groups)
 module ChatApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
-    config.i18n.default_locale = :ja
-    config.time_zone = 'Tokyo'
-    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
       config.load_defaults 6.0
       config.generators do |g|
         g.stylesheets false
@@ -25,5 +16,13 @@ module ChatApp
         g.helper false
         g.test_framework false
       end
+      config.i18n.default_locale = :ja
+      config.time_zone = 'Tokyo'
+      config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.   
   end
 end
