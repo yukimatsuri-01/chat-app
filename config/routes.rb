ea14a532b4a9eb619a1835230ea7b11117a666b4
yukimatsuri-01 <matsunariyukiko@gmail.com>
2020-09-01 12:47:47 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create] #チャットルームに属したメッセージ
+  end
+ 
 end
